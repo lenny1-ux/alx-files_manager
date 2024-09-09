91b31e1c94a82d22e3164c55401cccd141e79e88
lenny1-ux <leonardsammy01@gmail.com>
2024-09-08 20:20:47 -0700

    debug 2

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-const express = require('express');
+import express from 'express';
+import { env } from 'process';
+
+const mainRoute = require('./routes/index');
+
 const app = express();
-const port = process.env.PORT || 5000;
+const port = env.PORT || 5000;
+app.use(express.json());
+app.use(mainRoute);
+app.listen(port, '127.0.0.1');
 
-// Load routes
-const routes = require('./routes/index');
-app.use('/api', routes);
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+export default app;
