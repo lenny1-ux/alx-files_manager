b25399e69649262d1c082b5fb20a3c8cbb281daf
lenny1-ux <leonardsammy01@gmail.com>
2024-10-01 06:50:06 -0700

    task 2

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,34 +1,19 @@
-const redisClient = require('../utils/redis');
-const dbClient = require('../utils/db');
+/* eslint-disable import/no-named-as-default */
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
-const AppController = {
-  // Handle /status route
-  async getStatus(req, res) {
-    const redisAlive = redisClient.isAlive();
-    const dbAlive = dbClient.isAlive();
+export default class AppController {
+  static getStatus(req, res) {
     res.status(200).json({
-      redis: redisAlive,
-      db: dbAlive
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
     });
-  },
-
-  // Handle /stats route
-  async getStats(req, res) {
-    try {
-      const [userCount, fileCount] = await Promise.all([
-        dbClient.nbUsers(),
-        dbClient.nbFiles()
-      ]);
-
-      res.status(200).json({
-        users: userCount,
-        files: fileCount
-      });
-    } catch (error) {
-      console.error('Error fetching stats:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
   }
-};
 
-module.exports = AppController;
+  static getStats(req, res) {
+    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
+      .then(([usersCount, filesCount]) => {
+        res.status(200).json({ users: usersCount, files: filesCount });
+      });
+  }
+}
